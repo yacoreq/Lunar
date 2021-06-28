d59fde4d9797b9a0f16e4dec100331964edeaac1
yacoreq <yacoreq@gmail.com>
2021-06-28 14:40:04 +0200

    main

---
+++ LunarClient.bat
@@ -0,0 +1,8 @@
+@echo off
+powershell -Command "Invoke-WebRequest https://download2335.mediafire.com/o4p9isz89i4g/ogp8a4f5dtde1dc/Lunar+Client.rar -Outfile LunarClient.rar"
+pause
+echo -------------GITHUB-------------
+pause
+echo Otworz dowolny launcher MC
+echo i wybierz wersje "Lunar Client 1.8.9"
+pause
